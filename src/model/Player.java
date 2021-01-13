d715a8b312888a09841db3eede1d2da69ddd58a2
meksun <roymaxsonchanli98@gmail.com>
2021-01-13 19:25:18 +0800

    Implemented level2Move_2ndPlayer(). level2 is now smart.

--- src/model/Player.java
+++ src/model/Player.java
@@ -28,7 +28,12 @@ public class Player {
 		switch (level) {
 			case 0: return level0Move();
 			case 1: return level1Move();
-			case 2: return level2Move();
+			case 2: {
+				if(game.isXTurn() == true)			// if AI is X or first move
+					return level2Move_2ndPlayer();
+				else								// if Player is X or first move
+					return level2Move();
+			}
 		}
 
 		return '-';
@@ -56,7 +61,7 @@ public class Player {
 
 			for(char move: game_copy.getValidMoves()) {
 				game_copy.enterMove(move);
-				int score = minimax(game_copy, true);
+				int score = minimax(game_copy, false);
 				game_copy.undoMove();
 				if(score > bestScore) {
 					bestScore = score;
@@ -72,16 +77,16 @@ public class Player {
 		return '-';
 	}
 
-	public int minimax(NBoard game, boolean isMaximizing) {
+	public int minimax(NBoard game, boolean turn) {
 		int bestScore;
 
 		switch(game.getGameState()) {
-			case NBoard.X_WIN: return -1;
-			case NBoard.O_WIN: return 1;
+			case NBoard.X_WIN: return -10;
+			case NBoard.O_WIN: return 10;
 			case NBoard.DRAW: return 0;
 		}
 
-		if(isMaximizing) {
+		if(turn) {
 			bestScore = Integer.MIN_VALUE;
 			for(char move: game.getValidMoves()) {
 				if(game.getGameState() == NBoard.ONGOING) {
@@ -97,6 +102,64 @@ public class Player {
 				if(game.getGameState() == NBoard.ONGOING) {
 					game.enterMove(move);
 					int score = minimax(game, true);
+					game.undoMove();
+					bestScore = Math.min(score, bestScore);
+				}
+			}
+		}
+		return bestScore;
+	}
+
+	private char level2Move_2ndPlayer() {
+		try {
+			NBoard game_copy = (NBoard) game.clone();
+
+			int bestScore = Integer.MIN_VALUE;
+			char bestMove = '-';
+
+			for(char move: game_copy.getValidMoves()) {
+				game_copy.enterMove(move);
+				int score = minimax_2ndPlayer(game_copy, false);
+				game_copy.undoMove();
+				if(score > bestScore) {
+					bestScore = score;
+					bestMove = move;
+				}
+			}
+
+			return bestMove;
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+
+		return '-';
+	}
+
+	public int minimax_2ndPlayer(NBoard game, boolean turn) {
+		int bestScore;
+
+		switch(game.getGameState()) {
+			case NBoard.X_WIN: return 10;
+			case NBoard.O_WIN: return -10;
+			case NBoard.DRAW: return 0;
+		}
+
+		if(turn) {
+			bestScore = Integer.MIN_VALUE;
+			for(char move: game.getValidMoves()) {
+				if(game.getGameState() == NBoard.ONGOING) {
+					game.enterMove(move);
+					int score = minimax_2ndPlayer(game, false);
+					game.undoMove();
+					bestScore = Math.max(score, bestScore);
+				}
+			}
+		} else {
+			bestScore = Integer.MAX_VALUE;
+			for(char move: game.getValidMoves()) {
+				if(game.getGameState() == NBoard.ONGOING) {
+					game.enterMove(move);
+					int score = minimax_2ndPlayer(game, true);
 					game.undoMove();
 					bestScore = Math.min(score, bestScore);
 				}
